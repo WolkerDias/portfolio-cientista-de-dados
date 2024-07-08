3ea8f2bac2df1cea2fd2ac81e65787db8322d20f
WolkerDias <wolker.sd@hotmail.com>
2024-07-08 13:36:04 -0400

    Ajuste da função python para inserir gtag em subdiretorios

--- insert_gtag.py
+++ insert_gtag.py
@@ -7,8 +7,10 @@ def insert_gtag(file_path):
   Args:
     file_path: Caminho para o arquivo HTML.
   """
-  with open(file_path, 'r') as f:
-    soup = BeautifulSoup(f.read(), 'html.parser')
+  with open(file_path, 'r', encoding='utf-8') as f:
+    html_content = f.read()
+    # Parseia o HTML usando BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
 
   # Verifica se a tag <head> existe
   if soup.head:
@@ -31,16 +33,18 @@ def insert_gtag(file_path):
     head_tag.insert(len(head_tag.contents), script_tag)
 
   # Salva as alterações no arquivo
-  with open(file_path, 'w') as f:
+  with open(file_path, 'w', encoding='utf-8') as f:
     f.write(str(soup))
 
 def main():
-  """Função principal para iterar pelos arquivos HTML e inserir o código."""
+  """Função principal para percorrer recursivamente diretórios e inserir o código."""
   build_dir = os.path.join(os.getcwd(), '_build', 'html')
-  for filename in os.listdir(build_dir):
-    if filename.endswith('.html'):
-      file_path = os.path.join(build_dir, filename)
-      insert_gtag(file_path)
+
+  for root, _, filenames in os.walk(build_dir):
+    for filename in filenames:
+      if filename.endswith('.html'):
+        file_path = os.path.join(root, filename)
+        insert_gtag(file_path)
 
 if __name__ == '__main__':
   main()
